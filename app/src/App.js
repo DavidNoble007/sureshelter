4cd74705a965462013c302cfa1d8a76dc098c0be
DavidNoble <david.r.noble@outlook.com>
2019-03-21 11:08:27 -0700

    carousel

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import AppBar from "./components/Nav";
+import AutoPlaySwipeableViews from "./components/Carousel";
+
+
 class App extends Component {
+  
   render() {
   
     return (
@@ -12,6 +16,16 @@ class App extends Component {
     );
   }
 
+  render() {
+  
+    return (
+      <div className="App">
+      <AutoPlaySwipeableViews />
+       
+      </div>
+    );
+  }
+  
 }
 
 export default App;
