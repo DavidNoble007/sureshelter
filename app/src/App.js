3fe23e4ffc3455502e2881bb0ca858ed095c2ab3
DavidNoble <david.r.noble@outlook.com>
2019-03-21 14:05:48 -0700

    good

--- app/src/App.js
+++ app/src/App.js
@@ -1,30 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-<<<<<<< HEAD
 
 import CarouselBody from "./components/Carousel";
 
 
 
-=======
 import Navbar from "./components/Nav";
 import CardBody from "./components/Card";
->>>>>>> 729480e898cc386e553a45f0ff3861ade0d91957
 class App extends Component {
   
   render() {
   
     return (
       <div className="App">
-<<<<<<< HEAD
      
       <CarouselBody />
 
-=======
       <Navbar />
       <br></br>
       <CardBody />
->>>>>>> 729480e898cc386e553a45f0ff3861ade0d91957
       </div>
     );
   }
