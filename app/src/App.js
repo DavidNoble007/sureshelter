2a95e890b493abeed6da88815ac195eed421b915
Yasiin Muhammad <yasiinmuhammad@Yasiins-MacBook-Pro.local>
2019-03-21 14:15:38 -0700

    "y"

--- app/src/App.js
+++ app/src/App.js
@@ -8,34 +8,11 @@ class App extends Component {
   
     return (
       <div className="App">
-<<<<<<< HEAD
-     
-      <CarouselBody />
-
-=======
-      <Navbar />
+       <CarouselBody />
+       <Navbar />
       <br></br>
-<<<<<<< HEAD
-     
       </div>
     );
   }
-
-  render(){
-    return(
-    
-    <CardBody />
-   
-    );
-  }
-
-=======
-      <CardBody />
->>>>>>> 729480e898cc386e553a45f0ff3861ade0d91957
-      </div>
-    );
-  }
->>>>>>> 261a55dc4d29e4445a6c5a6c0976336eaedf4f85
 }
-
 export default App;
