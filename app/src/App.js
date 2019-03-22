961f42836ad19004130141ba3354bc2900dcc999
Yasiin Muhammad <yasiinmuhammad@Yasiins-MacBook-Pro.local>
2019-03-22 09:56:24 -0700

    YM

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import CarouselBody from "./components/Carousel";
 import Navbar from "./components/Nav";
-import CardBody from "./components/Card";
-class App extends Component {
-
-  render() {
+import Volunteer from "./Pages/Volunteer"
+import Donate from "./Pages/Donate"
+function App()  {
 
     return (
+      <Router>
       <div className="App">
         <Navbar />
         <br></br>
-        <CarouselBody />
+        {/* <CarouselBody /> */}
+        <Switch>
+          <Route exact path="/" component={CarouselBody} />
+          <Route exact path="/volunteer" component={Volunteer} />
+          <Route exact path="/donate" component={Donate} />
+          
+        </Switch>
       </div>
+      </Router>
     );
-  }
+  
 }
 export default App;
---
+++ app/src/Pages/Volunteer.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import CardBody from '../components/Card/index';
+import Col from 'react-bootstrap/Col'
+import Jumbotron from "../components/Jumbotron";
+function Volunteer() {
+  return (
+    <div>
+      {/* <Jumbotron fluid>
+      <Container>
+        <h1>Volunteer with us!</h1>
+        <h3>
+        Thank you for your interest in volunteering. With only a handful of programs around the valley, volunteers play a crucial role in delivering services every night to our cities homeless residents.
+
+    </h3>
+      </Container>
+    </Jumbotron> */}
+
+      <Container>
+        <Row><Col>
+          <CardBody />
+        </Col>
+          <Col>
+            <CardBody />
+          </Col>
+          <Col>       
+           <CardBody />
+          </Col>    
+          </Row>
+
+      </Container>
+    </div>
+
+  );
+}
+
+export default Volunteer;
