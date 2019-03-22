75ad5462b1b9454334fc97fe11cae62a83abce5e
Yasiin Muhammad <yasiinmuhammad@Yasiins-MacBook-Pro.local>
2019-03-22 13:19:30 -0700

    "ym"

--- app/src/components/Nav/index.js
+++ app/src/components/Nav/index.js
@@ -8,7 +8,6 @@ function ButtonAppBar(props) {
   const { classes } = props;
   return (
     <div className={classes}>
-<<<<<<< HEAD
       <Navbar bg="dark" variant="dark"><img
         src="https://i.ibb.co/7nm1t6j/d8ca65c3-d6f7-453c-ae19-eda013271331.png"
         width="30"
@@ -17,29 +16,17 @@ function ButtonAppBar(props) {
         alt="React Bootstrap logo"
       />
         <Navbar.Brand href="">SURESHELTER</Navbar.Brand>
-=======
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="/">SureShelter</Navbar.Brand>
->>>>>>> 0e34f541c3398b1313264f6c1da647eb3be08d5f
         <Nav className="mr-auto">
           <Link to="/">
             <Nav.Link href="/">Home</Nav.Link>
           </Link>
 
           <Link to="/volunteer">
-<<<<<<< HEAD
-            <Nav.Link href="/volunteer">Volunteer</Nav.Link>
-          </Link>
-
-          <Link to="/donate">
-            <Nav.Link href="/donate">Donations</Nav.Link>
-=======
             <Nav.Link href="#pricing">Volunteer</Nav.Link>
           </Link>
 
           <Link to="/donate">
             <Nav.Link href="#pricing">Donations</Nav.Link>
->>>>>>> 0e34f541c3398b1313264f6c1da647eb3be08d5f
           </Link>
         </Nav>
 
