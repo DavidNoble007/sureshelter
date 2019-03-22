70a7ff6fd7ac1f08fde0248bbe341df6c3f896ec
Yasiin Muhammad <yasiinmuhammad@Yasiins-MacBook-Pro.local>
2019-03-22 11:55:25 -0700

    YM

--- app/src/components/Nav/index.js
+++ app/src/components/Nav/index.js
@@ -10,20 +10,25 @@ function ButtonAppBar(props) {
   const { classes } = props;
   return (
     <div className={classes}>
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">SureShelter</Navbar.Brand>
+      <Navbar bg="dark" variant="dark"><img
+        src="https://i.ibb.co/7nm1t6j/d8ca65c3-d6f7-453c-ae19-eda013271331.png"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+      />
+        <Navbar.Brand href="#home">SURESHELTER</Navbar.Brand>
         <Nav className="mr-auto">
-        <Link to="/">
-          <Nav.Link href="#pricing">Home</Nav.Link>
-        </Link>
+          <Link to="/">
+            <Nav.Link href="#pricing">Home</Nav.Link>
+          </Link>
 
-        <Link to="/volunteer">
-          <Nav.Link href="#pricing">Volunteer</Nav.Link>
-        </Link>
+          <Link to="/volunteer">
+            <Nav.Link href="#pricing">Volunteer</Nav.Link>
+          </Link>
 
-        <Link to="/donate">
-          <Nav.Link href="#pricing">Donations</Nav.Link>
-        </Link>
+          <Link to="/donate">
+            <Nav.Link href="#pricing">Donations</Nav.Link>
+          </Link>
         </Nav>
         <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
