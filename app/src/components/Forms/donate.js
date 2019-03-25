6869cfd5cc5aafa799dcc458bd719d01e2b737bb
amyjosm <amyjosm@gmail.com>
2019-03-25 11:06:03 -0700

    errors

--- app/src/components/Forms/donate.js
+++ app/src/components/Forms/donate.js
@@ -1,7 +1,5 @@
 import React from "react";
-import Form from 'react-bootstrap/Form';
-import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
+import { Form, Col, Row } from 'react-bootstrap';
 
 function DonateForm({ children }) {
   return (
