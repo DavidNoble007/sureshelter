6c078a93a123ff0f2049bf1015a517f6426d548e
Yasiin Muhammad <yasiinmuhammad@Yasiins-MacBook-Pro.local>
2019-03-26 12:37:44 -0700

    y

--- app/src/components/Jumbotron/index.js
+++ app/src/components/Jumbotron/index.js
@@ -1,14 +1,15 @@
-import React, { Component } from "react";
-import {Jumbotron} from 'react-bootstrap';
-import React, {Component} from "react"
+import React from "react";
+import { Jumbotron, Grid } from 'react-bootstrap';
 
-class Jumbotron extends Component {
-  render(){
+function Jumbotron({ children }) {
   return (
-    <Jumbotron fluid>
-    <h1>Hello</h1>
-  </Jumbotron>
+    <Jumbotron>
+      <Grid>
+        <h1>Hello, world!</h1>
+        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+      </Grid>
+    </Jumbotron>
   );
-}};
+}
 
 export default Jumbotron;
