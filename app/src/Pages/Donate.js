546eced38821a5dba0b456a4bd4cad699ebba06d
amyjosm <amyjosm@gmail.com>
2019-03-26 11:37:25 -0700

    forms

--- app/src/Pages/Donate.js
+++ app/src/Pages/Donate.js
@@ -1,20 +1,29 @@
 import React, { Component } from "react";
-import { Container, Row, Col } from 'react-bootstrap';
-import CardBody from '../components/Card/index';
-import DonateForm from '../components/Forms/donate';
-import DonateCard from '../components/Card/DonateCard'
+import { Container, Row, Col } from "react-bootstrap";
+// import CardBody from '../components/Card/index';
+import DonateForm from "../components/Forms/donateForm";
+import DonateCard from "../components/Card/DonateCard";
+
 function Donate() {
   return (
-    <div style={{
-      backgroundImage: "url(" + "https://media.npr.org/assets/img/2018/01/09/gettyimages-901547744_wide-5be547c0dc69471823fd6808f0f83115a9db22e5-s800-c85.jpg" + ")",
-      backgroundPosition: 'center',
-      backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat',
-      height: 960
-    }}>
+    <div
+      style={{
+        backgroundImage:
+          "url(" +
+          "https://media.npr.org/assets/img/2018/01/09/gettyimages-901547744_wide-5be547c0dc69471823fd6808f0f83115a9db22e5-s800-c85.jpg" +
+          ")",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        height: 960
+      }}
+    >
       <Container>
         <Col>
-          <Row><h2 style={{ fontStyle: "Stencil Std, fantasy", }}>BUILDING A MOVEMENT TO END HOMELESSNESS</h2>
+          <Row>
+            <h2 style={{ fontStyle: "Stencil Std, fantasy" }}>
+              BUILDING A MOVEMENT TO END HOMELESSNESS
+            </h2>
             <DonateCard />
             <Col>
               <h3>DONATIONS</h3>
@@ -24,7 +33,6 @@ function Donate() {
         </Col>
       </Container>
     </div>
-
   );
 }
 
