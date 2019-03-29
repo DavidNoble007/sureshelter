75ba55c8d3ead46c3faff97aa7e8e766de9e6c01
DavidNoble <david.r.noble@outlook.com>
2019-03-29 10:07:04 -0700

    link cards on home page

--- app/src/components/Card/FactCardOne.js
+++ app/src/components/Card/FactCardOne.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Card, Container, Row, CardDeck, Col } from 'react-bootstrap';
+import { Card, Container, Row, CardDeck, Col, Nav } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import "./style.css";
 
 function FactCardOne(props) {
   const { classes } = props;
@@ -9,35 +11,55 @@ function FactCardOne(props) {
         <Row>
           <CardDeck>
             <Col>
-              <Card bg="dark" text="white" style={{ width: '14rem' }}>
-              <Card.Header>Fact #1</Card.Header>
-                <Card.Text>
-                Over half a million people are homeless.
-      </Card.Text>
+              <Card style={{ width: '14rem' }}>
+                <Link className="factCard" to="/volunteer">
+                  <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2015/06/18/23/01/wanderer-814222_1280.jpg" />
+                </Link>
+                <Card.Body>
+                  <Card.Text>
+                    Over half a million people are homeless.
+                 </Card.Text>
+                  <Link className="factCard" to="/volunteer">VOLUNTEER</Link>
+                </Card.Body>
               </Card>
             </Col>
             <Col>
-              <Card bg="dark" text="white" style={{ width: '14rem' }}>
-              <Card.Header>Fact #2</Card.Header>
-                <Card.Text>
-                One quarter of homeless people are children.
-      </Card.Text>
+              <Card style={{ width: '14rem' }}>
+                <Link className="factCard" to="/donate">
+                  <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2017/09/04/12/00/on-the-street-2713688_1280.jpg" />
+                </Link>
+                <Card.Body>
+                  <Card.Text>
+                    1/4 of homeless people are children.
+                 </Card.Text>
+                  <Link className="factCard" to="/donate">DONATIONS</Link>
+                </Card.Body>
               </Card>
             </Col>
             <Col>
-              <Card bg="dark" text="white" style={{ width: '14rem' }}>
-              <Card.Header>Fact #3</Card.Header>
-                <Card.Text>
-                Over 57,000 veterans are homeless every night.
-      </Card.Text>
+              <Card style={{ width: '14rem' }}>
+                <Link className="factCard" to="/signup">
+                  <Card.Img variant="top" src="https://images.unsplash.com/photo-1518398046578-8cca57782e17?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" />
+                </Link>
+                <Card.Body>
+                  <Card.Text>
+                    57,000 veterans are homeless every night.
+                 </Card.Text>
+                  <Link className="factCard" to="/signup">SIGN UP</Link>
+                </Card.Body>
               </Card>
             </Col>
             <Col>
-              <Card bg="dark" text="white" style={{ width: '14rem' }}>
-            <Card.Header>Fact #4</Card.Header>
-                <Card.Text>
-                1 in 5 homeless people suffers from mental illness.
-      </Card.Text>
+              <Card style={{ width: '14rem' }}>
+                <Link className="factCard" to="/login">
+                  <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2018/03/04/05/59/walls-3197310_1280.jpg" />
+                </Link>
+                <Card.Body>
+                  <Card.Text>
+                    1 in 5 homeless suffer from mental illness.
+                 </Card.Text>
+                  <Link className="factCard" to="/login">LOGIN</Link>
+                </Card.Body>
               </Card>
             </Col>
           </CardDeck>
