23644197952955ef92ee5ab3b4cbaa7471dd3a5c
amyjosm <amyjosm@gmail.com>
2019-03-29 11:50:10 -0700

    excess files deleted

--- app/src/state/modal/reducer.js
+++
@@ -1,9 +0,0 @@
-import { handleActions } from "redux-actions";
-import { changeModal, closeModal } from "./actions";
-
-const modalReducer = handleActions({
-  [changeModal]: (state, action) => action.payload,
-  [closeModal]: (state, action) => null
-}, null);
-
-export default modalReducer;
