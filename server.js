abe8928376d3d630d3e291aca6935221628b7ae9
DavidNoble <david.r.noble@outlook.com>
2019-04-01 10:50:53 -0700

    Deploying

--- server.js
+++ server.js
@@ -10,24 +10,24 @@ const nodemailer = require("nodemailer");
 // Passport configuration
 require("./services/passport");
 
-// Mongoose Connection
-const db = require("./config/connection");
-
-db(process.env.MONGODB_URI || "mongodb://localhost/sureshelter");
-
-// Express Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(require("morgan")("dev"));
-
 // Serve Static
 app.use(express.static("app/build"));
+
+// Express Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(require("morgan")("dev"));
 
 app.use(require("./routes/login-signup/index"));
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname + "/app/build/index.html"));
 });
+
+// Mongoose Connection
+const db = require("./config/connection");
+
+db(process.env.MONGODB_URI || "mongodb://localhost/sureshelter");
 
 app.listen(PORT, function() {
   console.log(`App listening on PORT ${PORT}`);
